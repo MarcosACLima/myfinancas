4f5c85ebd3ce9151be66852d143f3cdde66d4ff8
Marcos Lima <marcosanthony21@hotmail.com>
2020-04-21 12:08:43 -0300

    add class Lancamento e Enums

---
+++ src/main/java/com/dlima/myfinancas/model/entity/StatusLancamento.java
@@ -0,0 +1,9 @@
+package com.dlima.myfinancas.model.entity;
+
+public enum StatusLancamento {
+	
+	PENDENTE,
+	CANCELADO,
+	EFETIVADO
+
+}
---
+++ src/main/java/com/dlima/myfinancas/model/entity/TipoLancamento.java
@@ -0,0 +1,8 @@
+package com.dlima.myfinancas.model.entity;
+
+public enum TipoLancamento {
+	
+	RECEITA,
+	DESPESA
+
+}
