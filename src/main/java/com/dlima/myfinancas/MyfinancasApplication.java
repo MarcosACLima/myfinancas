082bce54e5c58cde6ce251b80046c5fd65eb6ca4
Marcos Lima <marcosanthony21@hotmail.com>
2020-04-21 11:17:33 -0300

    Projeto myfinancas - class Usuario, config. BD_PG

---
+++ src/main/java/com/dlima/myfinancas/MyfinancasApplication.java
@@ -0,0 +1,13 @@
+package com.dlima.myfinancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfinancasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dlima/myfinancas/MyfinancasApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dlima.myfinancas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyfinancasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
