1b464a77e69c7ffa8184702b0d34d3ddb2015ee5
Marcos Lima <marcosanthony21@hotmail.com>
2020-04-22 12:15:39 -0300

    Add H2 DB

--- src/test/java/com/dlima/myfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/dlima/myfinancas/model/repository/UsuarioRepositoryTest.java
@@ -5,12 +5,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.dlima.myfinancas.model.entity.Usuario;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@ActiveProfiles("test") // declarar que vai usar application-test
 public class UsuarioRepositoryTest {
 	
 	@Autowired
