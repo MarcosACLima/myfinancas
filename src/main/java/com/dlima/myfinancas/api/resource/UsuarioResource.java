520818fd656c40fc0856b417ad5c9164aa693629
Marcos Lima <marcosanthony21@hotmail.com>
2020-04-28 12:19:39 -0300

    Implementando o Endpoint de autenticar Usuario

--- src/main/java/com/dlima/myfinancas/api/resource/UsuarioResource.java
+++ src/main/java/com/dlima/myfinancas/api/resource/UsuarioResource.java
@@ -9,12 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dlima.myfinancas.api.dto.UsuarioDTO;
+import com.dlima.myfinancas.exception.ErroAutenticacao;
 import com.dlima.myfinancas.exception.RegraNegocioException;
 import com.dlima.myfinancas.model.entity.Usuario;
 import com.dlima.myfinancas.service.UsuarioService;
 
 @RestController
-@RequestMapping("/api/usuarios")
+@RequestMapping("/api/usuarios") // http://localhost:8080/api/usuarios
 public class UsuarioResource  /* UsuarioControler */ {
 	
 	private UsuarioService service;
@@ -23,9 +24,7 @@ public class UsuarioResource  /* UsuarioControler */ {
 		this.service = service;
 	}
 	
-	@PostMapping
 	public ResponseEntity salvar(@RequestBody UsuarioDTO dto) {
-		
 		Usuario usuario = Usuario.builder()
 				.nome(dto.getNome())
 				.email(dto.getEmail())
@@ -33,12 +32,21 @@ public class UsuarioResource  /* UsuarioControler */ {
 		
 		try {
 			Usuario usuarioSalvo = service.salvarUsuario(usuario);
-			return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
+			return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED); // return objeto
 			
 		} catch (RegraNegocioException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
+		}	
+	}
+	
+	@PostMapping("/autenticar")
+	public ResponseEntity autenticar(@RequestBody UsuarioDTO dto) {
+		try {
+			Usuario usuarioAutenticado = service.autenticar(dto.getEmail(), dto.getSenha());
+			return ResponseEntity.ok(usuarioAutenticado); 
+		} catch (ErroAutenticacao e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
 		}
-				
 	}
 
 }
