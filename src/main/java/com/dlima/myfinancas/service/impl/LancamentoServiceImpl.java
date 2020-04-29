d1403e6560dd864f9ff4231402e51aa4dcb0365c
Marcos Lima <marcosanthony21@hotmail.com>
2020-04-28 23:16:36 -0300

    Implementando metodos LancamentoServiceImpl
    - metodo buscar usado Example para fazer filtro na busca

--- src/main/java/com/dlima/myfinancas/service/impl/LancamentoServiceImpl.java
+++ src/main/java/com/dlima/myfinancas/service/impl/LancamentoServiceImpl.java
@@ -1,44 +1,63 @@
 package com.dlima.myfinancas.service.impl;
 
 import java.util.List;
+import java.util.Objects;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.ExampleMatcher.StringMatcher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dlima.myfinancas.model.entity.Lancamento;
 import com.dlima.myfinancas.model.enums.StatusLancamento;
+import com.dlima.myfinancas.model.repository.LancamentoRepository;
 import com.dlima.myfinancas.service.LancamentoService;
 
 @Service
 public class LancamentoServiceImpl implements LancamentoService {
+	
+	private LancamentoRepository repository;
+	
+	public LancamentoServiceImpl(LancamentoRepository repository) {
+		this.repository = repository;
+	}
 
 	@Override
+	@Transactional
 	public Lancamento salvar(Lancamento lancamento) {
-		// TODO Auto-generated method stub
-		return null;
+		return repository.save(lancamento);
 	}
 
 	@Override
+	@Transactional
 	public Lancamento atualizar(Lancamento lancamento) {
-		// TODO Auto-generated method stub
-		return null;
+		Objects.requireNonNull(lancamento.getId()); // deve passar um lancamento existente com id
+		return repository.save(lancamento);
 	}
 
 	@Override
+	@Transactional
 	public void deletar(Lancamento lancamento) {
-		// TODO Auto-generated method stub
-		
+		Objects.requireNonNull(lancamento.getId());
+		repository.delete(lancamento);
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public List<Lancamento> buscar(Lancamento lancamentoFiltro) {
-		// TODO Auto-generated method stub
-		return null;
+		Example example = Example.of(lancamentoFiltro, 
+				ExampleMatcher.matching()
+					.withIgnoreCase() // IgnoraCase - ignorar maiusculas e minusculas
+					.withStringMatcher(StringMatcher.STARTING)); // contenha tal string
+
+		return repository.findAll(example);
 	}
 
 	@Override
 	public void atualizarStatus(Lancamento lancamento, StatusLancamento status) {
-		// TODO Auto-generated method stub
-		
+		lancamento.setStatus(status);
+		atualizar(lancamento);
 	}
 
 }
