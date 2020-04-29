84bd50cc430f15d5487bfd3ea03b3590603fabff
Marcos Lima <marcosanthony21@hotmail.com>
2020-04-28 22:07:07 -0300

    Criando Interface LancamentoService e classe de Implementacao da mesma

---
+++ src/main/java/com/dlima/myfinancas/service/impl/LancamentoServiceImpl.java
@@ -0,0 +1,44 @@
+package com.dlima.myfinancas.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.dlima.myfinancas.model.entity.Lancamento;
+import com.dlima.myfinancas.model.enums.StatusLancamento;
+import com.dlima.myfinancas.service.LancamentoService;
+
+@Service
+public class LancamentoServiceImpl implements LancamentoService {
+
+	@Override
+	public Lancamento salvar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Lancamento atualizar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deletar(Lancamento lancamento) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Lancamento> buscar(Lancamento lancamentoFiltro) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void atualizarStatus(Lancamento lancamento, StatusLancamento status) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
