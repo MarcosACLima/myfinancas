29b4d0c2e9e5aee67aadf1bed6d642d7e1c08e98
Marcos Lima <marcosanthony21@hotmail.com>
2020-04-29 17:23:28 -0300

    Implementando Endpoint de Deletar Lancamento
    HttpStatus.NO_CONTENT

--- src/main/java/com/dlima/myfinancas/api/resource/LancamentoResource.java
+++ src/main/java/com/dlima/myfinancas/api/resource/LancamentoResource.java
@@ -2,6 +2,7 @@ package com.dlima.myfinancas.api.resource;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -57,6 +58,15 @@ public class LancamentoResource {
 				"Lançamento não encontrado na base de dados.", 
 				HttpStatus.BAD_REQUEST
 		));
+	}
+	
+	@DeleteMapping("id")
+	public ResponseEntity deletar(@PathVariable("id") Long id) {
+		return service.obterPorId(id).map( entidade -> {
+			service.deletar(entidade);
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
+		}).orElseGet( 
+			() -> new ResponseEntity("Lançamento não encontrado na base de dados.", HttpStatus.BAD_REQUEST));
 	}
 	
 	/* Converter DTO em Lancamento */
