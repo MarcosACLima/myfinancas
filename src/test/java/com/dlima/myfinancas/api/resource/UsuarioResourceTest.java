4247181445edf1092cf386be027c64939c4ab1c8
Marcos Lima <marcosanthony21@hotmail.com>
2020-05-04 11:31:28 -0300

    Implementando teste de autenticar usuario na camada Rest Controller

---
+++ src/test/java/com/dlima/myfinancas/api/resource/UsuarioResourceTest.java
@@ -0,0 +1,74 @@
+package com.dlima.myfinancas.api.resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.dlima.myfinancas.api.dto.UsuarioDTO;
+import com.dlima.myfinancas.model.entity.Usuario;
+import com.dlima.myfinancas.service.LancamentoService;
+import com.dlima.myfinancas.service.UsuarioService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+@WebMvcTest(controllers = UsuarioResource.class) // subir contexto  para test
+@AutoConfigureMockMvc
+public class UsuarioResourceTest {
+	
+	static final String API = "/api/usuarios";
+	static final MediaType JSON = MediaType.APPLICATION_JSON;
+	
+	@Autowired
+	MockMvc mvc;
+	
+	@MockBean
+	UsuarioService service;
+	
+	@MockBean
+	LancamentoService lancamentoService;
+	
+	@Test
+	public void deveAutenticarUmUsuario() throws Exception {
+		// cenario
+		// simulando back end
+		String email = "usuario@email.com";
+		String senha = "123";
+		
+		UsuarioDTO dto = UsuarioDTO.builder().email(email).senha(senha).build();
+		Usuario usuario = Usuario.builder().id(1l).email(email).senha(senha).build();
+		
+		Mockito.when(service.autenticar(email, senha)).thenReturn(usuario);
+		
+		String json = new ObjectMapper().writeValueAsString(dto); // transformar objeto em uma String Json
+		
+		// execucao e verificacao
+		// simulando front end
+		MockHttpServletRequestBuilder requisicao = MockMvcRequestBuilders
+													.post(API.concat("/autenticar"))
+													.accept(JSON)
+													.contentType(JSON)
+													.content(json);
+		
+		mvc
+			.perform(requisicao)
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.jsonPath("id").value(usuario.getId()))
+			.andExpect(MockMvcResultMatchers.jsonPath("nome").value(usuario.getNome()))
+			.andExpect(MockMvcResultMatchers.jsonPath("email").value(usuario.getEmail()))
+		;
+		
+	}
+
+}
