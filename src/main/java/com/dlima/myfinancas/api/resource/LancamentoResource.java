d10504162227019c58ab5f9a157cdd1dc110e113
Marcos Lima <marcosanthony21@hotmail.com>
2020-07-11 20:11:52 -0300

    Implementado end point de buscar lancamento por Id
    - Usado para carregar dados no lancamento no formuario de cadastro/edicao do front end

--- src/main/java/com/dlima/myfinancas/api/resource/LancamentoResource.java
+++ src/main/java/com/dlima/myfinancas/api/resource/LancamentoResource.java
@@ -122,6 +122,13 @@ public class LancamentoResource {
 			new ResponseEntity("Lançamento não encontrado na base de dados.", HttpStatus.BAD_REQUEST));
 	}
 	
+	@GetMapping("{id}")
+	public ResponseEntity obterLancamento(@PathVariable("id") Long id) {
+		return service.obterPorId(id)
+				.map( lancamento -> new ResponseEntity(converter(lancamento), HttpStatus.OK))
+				.orElseGet( () -> new ResponseEntity(HttpStatus.NOT_FOUND));
+	}
+	
 	/* Converter DTO em Lancamento */
 	private Lancamento converter(LancamentoDTO dto) { 
 		Lancamento lancamento = new Lancamento();
@@ -146,6 +153,20 @@ public class LancamentoResource {
 		}
 		
 		return lancamento;
+	}
+	
+	/* Converter Lancamento em DTO */
+	private LancamentoDTO converter(Lancamento lancamento) {
+		return LancamentoDTO.builder()
+				.id(lancamento.getId())
+				.descricao(lancamento.getDescricao())
+				.valor(lancamento.getValor())
+				.mes(lancamento.getMes())
+				.ano(lancamento.getAno())
+				.status(lancamento.getStatus().name()) // .name transforma ENUM em String
+				.tipo(lancamento.getTipo().name())
+				.usuario(lancamento.getUsuario().getId())
+				.build();
 	}
 	
 }
